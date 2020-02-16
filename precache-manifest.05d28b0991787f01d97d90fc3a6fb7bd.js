5e18e323ab403e2daa26db9538701f8c919f76cf
unknown <an.hommers@gmail.com>
2020-02-16 20:49:15 +0100

    Updates

---
+++ precache-manifest.05d28b0991787f01d97d90fc3a6fb7bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27868cc0543bb2aea71e0eacd08d08ed",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "f250600e8b440e0e58fc",
+    "url": "/FaceRecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6853bd577ed7b7804fd6",
+    "url": "/FaceRecognition/static/css/main.816a6b22.chunk.css"
+  },
+  {
+    "revision": "f250600e8b440e0e58fc",
+    "url": "/FaceRecognition/static/js/2.bbf22504.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/FaceRecognition/static/js/2.bbf22504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6853bd577ed7b7804fd6",
+    "url": "/FaceRecognition/static/js/main.325784ea.chunk.js"
+  },
+  {
+    "revision": "2e6c1cb8e8a5dd3d5a96",
+    "url": "/FaceRecognition/static/js/runtime-main.e9b5e175.js"
+  }
+]);
